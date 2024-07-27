b77f0566b1c1d7741ac6dc505e4866fb1ae97423
Wayne-dot <wayneting.wc@gmail.com>
2024-07-27 17:29:38 -0400

    project setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
